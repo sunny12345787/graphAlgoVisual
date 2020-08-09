334950f176d33de078622d8784c53512951a6328
sunny kumar <sunny.kumar06542@gmail.com>
2020-08-09 19:43:06 +0530

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import PathfindingVisual from './PathfindingVisual/PathfindingVisual';
+
+function App() {
+  return (
+    <div className="App">
+      <PathfindingVisual></PathfindingVisual>
+    </div>
+  );
+}
+
+export default App;
